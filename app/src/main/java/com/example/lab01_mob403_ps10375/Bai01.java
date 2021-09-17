70de1343d8e12654509883eeaaf86668b32a3d95
quangthuan01 <thuannqIt01@gmail.com>
2021-09-17 17:20:01 +0700

    Lab01_Mob403_PS10375

---
+++ app/src/main/java/com/example/lab01_mob403_ps10375/Bai01.java
@@ -0,0 +1,14 @@
+package com.example.lab01_mob403_ps10375;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai01);
+    }
+}
